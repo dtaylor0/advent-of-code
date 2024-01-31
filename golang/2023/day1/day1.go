a41386e687cc750bf2f178dd338e503ea7758902
Drew Taylor <drewtaylorm@gmail.com>
2024-01-30 19:43:03 -0500

    removing functions, starting to fix day 2

--- golang/2023/day1/day1.go
+++ golang/2023/day1/day1.go
@@ -4,7 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"os"
-	"unicode/utf8"
+	"strings"
 )
 
 func partOne() {
@@ -44,27 +44,30 @@ func partOne() {
 	fmt.Printf("Total Sum: %d\n", totalSum)
 }
 
-func runeIsInt(in rune) bool {
-	return in >= '0' && in <= '9'
-}
-
-func findFirstInt(s *string) int {
-	for idx, runeVal := range *s {
-		if runeIsInt(runeVal) {
-			return idx
-		}
+func stringToInt(s string) int {
+	digitsMap := map[string]int{
+		"zero":  0,
+		"one":   1,
+		"two":   2,
+		"three": 3,
+		"four":  4,
+		"five":  5,
+		"six":   6,
+		"seven": 7,
+		"eight": 8,
+		"nine":  9,
+		"0":     0,
+		"1":     1,
+		"2":     2,
+		"3":     3,
+		"4":     4,
+		"5":     5,
+		"6":     6,
+		"7":     7,
+		"8":     8,
+		"9":     9,
 	}
-	return -1
-}
-
-func findLastInt(s *string) int {
-	i := -1
-	for idx, runeVal := range *s {
-		if runeIsInt(runeVal) {
-			i = idx
-		}
-	}
-	return i
+	return digitsMap[s]
 }
 
 func partTwo() {
@@ -75,51 +78,55 @@ func partTwo() {
 	defer file.Close()
 
 	totalSum := 0
-	// digitMap := map[string]int{
-	// 	"zero":  0,
-	// 	"one":   1,
-	// 	"two":   2,
-	// 	"three": 3,
-	// 	"four":  4,
-	// 	"five":  5,
-	// 	"six":   6,
-	// 	"seven": 7,
-	// 	"eight": 8,
-	// 	"nine":  9,
-	// }
 
 	scanner := bufio.NewScanner(file)
 	for scanner.Scan() {
 		text := scanner.Text()
-		first, _ := utf8.DecodeRuneInString(text)
-		var firstValue int
-		last, _ := utf8.DecodeLastRuneInString(text)
-		var lastValue int
+		firstIdx := -1
+		lastIdx := -1
+		var firstVal string
+		var lastVal string
 
-		if runeIsInt(first) {
-			firstValue = int(first - '0')
-		} else {
-			print("else")
+		for _, digit := range []string{"1", "2", "3", "4", "5", "6", "7", "8", "9"} {
+			currIdxFirst := strings.Index(text, digit)
+			if currIdxFirst > -1 && currIdxFirst < firstIdx {
+				firstIdx = currIdxFirst
+				firstVal = string(digit)
+			}
+			currIdxLast := strings.LastIndex(text, digit)
+			if currIdxLast > -1 && currIdxLast > lastIdx {
+				lastIdx = currIdxLast
+				lastVal = string(digit)
+			}
 		}
-		if runeIsInt(last) {
-			lastValue = int(last - '0')
-		} else {
-			print("else")
+
+		for _, digit := range []string{"one", "two", "three", "four", "five", "six", "seven", "eight", "nine"} {
+			currIdxFirst := strings.Index(text, digit)
+			if currIdxFirst > -1 && currIdxFirst < firstIdx {
+				firstIdx = currIdxFirst
+				firstVal = string(digit)
+			}
+			currIdxLast := strings.LastIndex(text, digit)
+			if currIdxLast > -1 && currIdxLast > lastIdx {
+				lastIdx = currIdxLast
+				lastVal = string(digit)
+			}
 		}
+
+		firstValue := stringToInt(firstVal)
+		lastValue := stringToInt(lastVal)
+
 		totalSum += 10*firstValue + lastValue
-	}
 
+	}
 	if err := scanner.Err(); err != nil {
 		panic(err)
 	}
 
-	fmt.Printf("Total Sum: %d\n", totalSum)
+	fmt.Printf("Total Sum Part 2: %d\n", totalSum)
 }
 
 func main() {
 	partOne()
-	inputs := []string{"djfkdlsjf4jkfldsjdf", "3jfkd", "kdjsfl8", "jdkfls"}
-	for i := 0; i < len(inputs); i++ {
-		fmt.Printf("%s: %d\n", inputs[i], findFirstInt(&inputs[i]))
-	}
+	partTwo()
 }
