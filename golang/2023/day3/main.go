aa6314b3f6c66b55dcdf364109fad49eedc2cd53
Drew Taylor <drewtaylorm@gmail.com>
2024-03-06 01:49:22 -0500

    completed day 3 part 2

--- golang/2023/day3/main.go
+++ golang/2023/day3/main.go
@@ -33,8 +33,15 @@ func get(lines *[]string, i int, j int, y int, x int) (byte, error) {
 	return (*lines)[i+y][j+x], nil
 }
 
-func main() {
-	fmt.Println("Day 3")
+func getNumber(s string) int {
+	val, err := strconv.Atoi(s)
+	if err != nil {
+		log.Fatal(err)
+	}
+	return val
+}
+
+func part1() {
 	dirs := [][]int{
 		{1, -1}, {1, 0}, {1, 1},
 		{0, -1}, {0, 1},
@@ -65,12 +72,7 @@ func main() {
 			isNumber = charIsNumber(char)
 
 			if !isNumber && !checkIfPart {
-				val, err := strconv.Atoi(currNumber)
-				if err != nil {
-					log.Fatal(err)
-				}
-
-				sum += val
+				sum += getNumber(currNumber)
 				checkIfPart = true
 			}
 
@@ -101,15 +103,118 @@ func main() {
 		}
 
 		if isNumber && !checkIfPart {
-			val, err := strconv.Atoi(currNumber)
-			if err != nil {
-				log.Fatal(err)
-			}
-
-			sum += val
+			sum += getNumber(currNumber)
 			checkIfPart = true
 		}
 	}
 
 	fmt.Println("Part 1:", sum)
+}
+
+func contains(s []int, v int) bool {
+	for _, val := range s {
+		if val == v {
+			return true
+		}
+	}
+	return false
+}
+
+func addNumToGearTracker(num int, i int, j int, gearTracker map[[2]int][]int) {
+	idx := [2]int{i, j}
+	if gearTracker[idx] != nil && !contains(gearTracker[idx], num) {
+		gearTracker[idx] = append(gearTracker[idx], num)
+	} else {
+		gearTracker[idx] = []int{num}
+	}
+}
+
+func part2() {
+	dirs := [][]int{
+		{1, -1}, {1, 0}, {1, 1},
+		{0, -1}, {0, 1},
+		{-1, -1}, {-1, 0}, {-1, 1},
+	}
+
+	file, err := os.Open("./input.txt")
+	if err != nil {
+		panic(err)
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+
+	gearTracker := make(map[[2]int][]int)
+	for i := 0; i < len(lines); i++ {
+		line := lines[i]
+		isNumber := false
+		checkIfPart := true
+		currNumber := ""
+		var gears [][2]int
+
+		for j := 0; j < len(line); j++ {
+			char, _ := get(&lines, i, j, 0, 0)
+			isNumber = charIsNumber(char)
+
+			if !isNumber && !checkIfPart {
+				for _, gear := range gears {
+					addNumToGearTracker(getNumber(currNumber), gear[0], gear[1], gearTracker)
+				}
+                gears = [][2]int{}
+				checkIfPart = true
+			}
+
+			if !isNumber {
+				currNumber = ""
+			}
+
+			if isNumber && checkIfPart {
+				isPart := false
+				for _, dir := range dirs {
+					dirChar, err := get(&lines, i, j, dir[0], dir[1])
+					if err != nil {
+						continue
+					}
+					if !isDot(dirChar) && !charIsNumber(dirChar) {
+						isPart = true
+						gears = append(gears, [2]int{i + dir[0], j + dir[1]})
+					}
+				}
+				if isPart {
+					checkIfPart = false
+				}
+			}
+
+			if isNumber {
+				currNumber += string([]byte{char})
+			}
+		}
+
+		if isNumber && !checkIfPart {
+            for _, gear := range gears {
+                addNumToGearTracker(getNumber(currNumber), gear[0], gear[1], gearTracker)
+            }
+            gears = [][2]int{}
+			checkIfPart = true
+		}
+	}
+
+	sum := 0
+	for _, adj := range gearTracker {
+		if len(adj) == 2 {
+			sum += adj[0] * adj[1]
+		}
+	}
+
+	fmt.Println("Part 2:", sum)
+}
+
+func main() {
+	fmt.Println("Day 3")
+	part1()
+	part2()
 }
