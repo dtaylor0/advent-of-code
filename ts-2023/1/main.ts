39f08ef34449bec9ba47b9d99aeea43809321a47
Drew Taylor <drewtaylorm@gmail.com>
2024-03-12 18:19:16 -0400

    part1 function, formatting

--- ts-2023/1/main.ts
+++ ts-2023/1/main.ts
@@ -1,21 +1,26 @@
-import { readFileSync } from 'fs';
+import { readFileSync } from "fs";
 
-console.log("Day 1")
+console.log("Day 1");
 
 function getValue(line: string): number {
     const reg = /(\d)/g;
-    const matches = [...line.matchAll(reg)]
+    const matches = [...line.matchAll(reg)];
     if (!matches || matches.length === 0) {
         throw new Error("No digit");
     }
 
-    const first = matches[0][0]
-    const last = matches[matches.length - 1][0][0]
+    const first = matches[0][0];
+    const last = matches[matches.length - 1][0][0];
 
     return parseInt(first + last);
 }
 
-const lines = readFileSync("input.txt").toString().trim().split("\n");
-let sum = lines.reduce((acc: number, line: string): number => { return acc + getValue(line) }, 0)
-console.log("Part 1: %d", sum)
+function part1() {
+    const lines = readFileSync("input.txt").toString().trim().split("\n");
+    let sum = lines.reduce((acc: number, line: string): number => {
+        return acc + getValue(line);
+    }, 0);
+    console.log("Part 1: %d", sum);
+}
 
+part1();
