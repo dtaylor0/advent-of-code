489e91c2444b406fd102f9ee7217143e96fe4ac6
Drew Taylor <drewtaylorm@gmail.com>
2024-03-14 16:29:04 -0400

    reduce further

--- ts-2023/4/main.ts
+++ ts-2023/4/main.ts
@@ -8,28 +8,21 @@ function part1() {
     const lines = getLines("input.txt");
     let res = lines
         .map((line: string): number => {
-            const game = line.split(": ")[1].trim();
-            const winners = game
-                .split(" | ")[0]
-                .split(/[ ]+/)
-            const cards = game
-                .split(" | ")[1]
-                .split(/[ ]+/)
+            const card = line.split(": ")[1].trim().split(" | ");
+            const winners = card[0].split(/\s+/);
+            const nums = card[1].split(/\s+/);
 
-            let sum = cards.reduce(
-                (acc, card) => acc + (winners.includes(card) ? 1 : 0),
+            let sum = nums.reduce(
+                (acc, card) => acc + Number(winners.includes(card)),
                 0,
             );
-            if (sum > 0) {
-                return 1 * 2 ** (sum - 1);
-            }
-            return 0;
+            return sum > 0 ? 1 * 2 ** (sum - 1) : 0;
         })
         .reduce((acc, item) => acc + item, 0);
     console.log("Part 1: ", res);
 }
 
-function part2() { }
+function part2() {}
 
 console.log("Day 4");
 part1();
