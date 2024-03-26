22101c51a3a225c98feb1e846630acb7530f3bc4
Drew Taylor <drewtaylorm@gmail.com>
2024-03-26 09:27:32 -0400

    rust day2 complete

--- rust-2023/2/main.rs
+++ rust-2023/2/main.rs
@@ -15,11 +15,18 @@ struct Game {
 
 impl From<&str> for Game {
     fn from(value: &str) -> Self {
-        let game_and_rounds: Vec<&str> = value.split(": ").collect();
-        let game_id: u32 = game_and_rounds[0].split_whitespace().collect::<Vec<&str>>()[1]
+        let mut game_and_rounds = value.split(": ");
+        let game_id: u32 = game_and_rounds
+            .next()
+            .unwrap()
+            .split_whitespace()
+            .nth(1)
+            .unwrap()
             .parse()
             .unwrap();
-        let rounds = game_and_rounds[1]
+        let rounds = game_and_rounds
+            .next()
+            .unwrap()
             .split("; ")
             .map(|round| {
                 let mut r = Round {
@@ -28,15 +35,13 @@ impl From<&str> for Game {
                     blue: 0,
                 };
                 for color in round.split(", ") {
-                    let color_count = color.split_whitespace().collect::<Vec<&str>>();
-                    let count: u32 = color_count[0].parse().unwrap();
-                    let color = color_count[1];
-                    if color == "red" {
-                        r.red = count;
-                    } else if color == "green" {
-                        r.green = count;
-                    } else if color == "blue" {
-                        r.blue = count;
+                    let mut color_count = color.split_whitespace();
+                    let count: u32 = color_count.next().unwrap().parse().unwrap();
+                    match color_count.next().unwrap() {
+                        "red" => r.red = count,
+                        "green" => r.green = count,
+                        "blue" => r.blue = count,
+                        _ => {}
                     }
                 }
                 r
@@ -46,22 +51,38 @@ impl From<&str> for Game {
     }
 }
 
-fn part1() {
-    let contents = fs::read_to_string("input.txt").unwrap();
+fn part1(contents: &str) {
     let games = contents.lines().map(|line| Game::from(line));
-
     let sum: u32 = games
-        .map(|game| {
-            let maxred = game.rounds.iter().max_by_key(|r| r.red).unwrap().red;
-            let maxgreen = game.rounds.iter().max_by_key(|r| r.green).unwrap().green;
-            let maxblue = game.rounds.iter().max_by_key(|r| r.blue).unwrap().blue;
-            maxred * maxgreen * maxblue
+        .filter(|game| {
+            let (maxred, maxgreen, maxblue) =
+                game.rounds.iter().fold((0, 0, 0), |(mr, mg, mb), r| {
+                    (mr.max(r.red), mg.max(r.green), mb.max(r.blue))
+                });
+            return maxred <= 12 && maxgreen <= 13 && maxblue <= 14;
         })
+        .map(|game| game.game_id)
         .sum();
     println!("Part 1: {}", sum);
 }
 
+fn part2(contents: &str) {
+    let games = contents.lines().map(|line| Game::from(line));
+    let sum: u32 = games
+        .map(|game| {
+            let (maxred, maxgreen, maxblue) =
+                game.rounds.iter().fold((0, 0, 0), |(mr, mg, mb), r| {
+                    (mr.max(r.red), mg.max(r.green), mb.max(r.blue))
+                });
+            maxred * maxgreen * maxblue
+        })
+        .sum();
+    println!("Part 2: {}", sum);
+}
+
 fn main() {
+    let contents = fs::read_to_string("input.txt").unwrap();
     println!("Day 2");
-    part1();
+    part1(&contents);
+    part2(&contents);
 }
